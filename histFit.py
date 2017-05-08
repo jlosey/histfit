b98dbc8ddcaa6846c7aefe04d000ab962282ac32
James Losey <jamesllosey@gmail.com>
2017-05-08 18:47:57 +1000

    Hist fit is now integrating over area of fit and printing ratio of area.

--- histFit.py
+++ histFit.py
@@ -2,10 +2,12 @@
 import glob
 import numpy as np
 from scipy.stats import normaltest
+from scipy.integrate import quad 
 import matplotlib.pylab as plt
 
-flist = sorted(glob.glob("../v3/gClst_0-0-0.70-*-25000.dat"))
-fig=plt.figure(figsize=(12,10))
+flist = sorted(glob.glob("../v3/gClst_0-0-*-0.30-25000.dat"),reverse=True)
+fig=plt.figure(1,figsize=(12,10))
+areaList = []
 count = 1
 for fl in flist:
 	n = []
@@ -13,8 +15,8 @@ for fl in flist:
 	pSn = []
 	clstCount = []
 	pClst = []
-	temp = fl[10:14]
-	dens = fl[15:19]
+	temp = fl[16:20]
+	dens = fl[21:25]
 	f = open(fl,'r')
 
 	for line in f:
@@ -27,6 +29,7 @@ for fl in flist:
 			pClst.append(float(li.split()[4]))
 	f.close()
 	X = np.linspace(0,20,200)
+	xA = np.arange(0,21)
 	nAr = np.asarray(n)
 	nAr2 = nAr[1:]
 
@@ -48,15 +51,19 @@ for fl in flist:
 	widthC2 = np.sqrt(np.abs(np.sum((nAr2-avgC2)**2*dataC2)/np.sum(dataC2)))
 	fitC = lambda t : maxDataC*np.exp(-(t-avgC)**2/(2*widthC**2))
 	fitC2 = lambda t : maxDataC2*np.exp(-(t-avgC2)**2/(2*widthC2**2))
-
+	area,aerr = quad(fitC,0,20)
+	area2,a2err = quad(fitC2,0,20)
+	areaList.append((temp,dens,area,aerr,area2,a2err,area/area2))
+	#print("Temp = {0} Rho = {1}".format(temp, dens))
+	#print("Area = {0}, area2 = {1}, simA = {2}".format(sum(fitC(X)),c2A,sum(dataC[1:])))
 	#test = normaltest(data)
 	#test2 = normaltest(data2-x)
 	#print test,test2
 	#width = np.sqrt(np.abs(np.sum((nAr2-x)**2*data2)/np.sum(data2)))
 
 	#plt.bar(nAr-0.5,dataS,width=1)
-	ax = fig.add_subplot(2,2,count)
-	ax.set_title("T=%s Dens=%s"%(temp,dens))
+	ax = fig.add_subplot(3,2,count)
+	ax.set_title("T={0} Dens={1}".format(temp,dens))
 	ax.set_xlim([0,20])
 	ax.set_xlabel("n")
 	ax.set_ylabel("P[n]")
@@ -67,6 +74,10 @@ for fl in flist:
 	if count==1:
 		ax.legend(loc="upper right")
 	count = count+1
+
+areaList = np.asarray(areaList)
+fig = plt.figure(2)
+plt.plot(areaList[:,0],areaList[:,6])
 #plt.ylim([0,0.2])
-#plt.show()
-plt.savefig("%s-nearN.png"%temp)
+plt.show()
+#plt.savefig("%s-nearN.png"%temp)
